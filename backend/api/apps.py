3c4e4409126388c75518d3647eec171672a71e9c
John Mugambi Ndeke <mugambindeke@gmail.com>
2025-05-16 06:09:56 -0700

    Update apps.py

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -1,1 +1,12 @@
+# backend/api/apps.py
+from django.apps import AppConfig
 
+class ApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'api'
+
+    def ready(self):
+        import api.signals # If you have signals
+        # You can also initialize things here if needed when Django starts
+        # (but heavy initializations are better in specific modules or management commands)
+        pass
